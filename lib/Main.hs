0d3884c5e81685cefa4d7109c128023684674848
Scott Lawrence <bytbox@gmail.com>
2012-01-03 11:45:15 -0500

    Updating Main.hs

--- lib/Main.hs
+++ lib/Main.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE  #-}
 
-module Main
- where
+module Main where
 
 import EVAN
-import TupleTH
 
 main = evanMain
+
 _Events1 = Events 
 
 _Return1 = Return __pipe_0
+
 {- Hello, world! -}
 
-__pipe_0 = $(nth 0 _Events1)
+__pipe_0 = (\ (x) -> x) _Events1
+
