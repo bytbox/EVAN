c0c4d56461878a20fdc6ff1c9c42855642de0260
Scott Lawrence <bytbox@gmail.com>
2012-01-08 13:00:05 -0500

    And counting works!

--- lib/Main.hs
+++ lib/Main.hs
@@ -6,9 +6,13 @@ main = _Return1
 
 _Events1 = _Events 
 
-_Return1 = _Return __pipe_0
+_Return1 = _Return __pipe_1
 
 {- Hello, world! -}
 
 __pipe_0 = (\ (x) -> x) _Events1
+
+__pipe_1 = (\ (x) -> x) _sbolwlorsdkb
+
+_sbolwlorsdkb = _Count __pipe_0
 
