75f51d120c0d23a3a0da4aebdedcd032479b8342
Scott Lawrence <bytbox@gmail.com>
2012-01-16 11:31:59 -0500

    Ditching haskell json2hs; created stub json2hs.py

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 pushd . > /dev/null
-cd json2hs
-make
-popd > /dev/null
-
-pushd . > /dev/null
 cd lib
 cabal configure
 cabal install
@@ -13,6 +8,7 @@ popd > /dev/null
 
 mkdir -p bin
 rm -f bin/*
-ln -s `pwd`/json2hs/json2hs `pwd`/scripts/evan-compile bin
+ln -s `pwd`/json2hs/json2hs.py bin/json2hs
+ln -s `pwd`/scripts/evan-compile bin/evan-compile
 tools/merge.pl gui/evan.py > bin/evan
 chmod +x bin/evan
