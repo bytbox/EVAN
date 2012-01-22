c86dba6a47466d9c48c602cecf384a91ecb02ada
Scott Lawrence <bytbox@gmail.com>
2012-01-21 21:46:48 -0500

    Using new Main.hs, to start on histograms

--- lib/Main.hs
+++ lib/Main.hs
@@ -1,18 +1,15 @@
 module Main where
-
 import EVAN
-
-main = _Return1
-
-_Events1 = _Events ()
-
-_Return1 = _Return __pipe_1
-
-{- Hello, world! -}
-
-__pipe_0 = (\ (x) -> x) _Events1
-
-__pipe_1 = (\ (x) -> x) _sbolwlorsdkb
-
-_sbolwlorsdkb = _Count __pipe_0
-
+main = do
+  let _Events1 = _Events ()
+  let __pipe_5 = (\ (x) -> x) _Events1
+  _rpsdfspgrjnu <- __pipe_5
+  let __pipe_6 = (\ (x) -> x) _rpsdfspgrjnu
+  let _lmymensnxnpt = _Jets (__pipe_6)
+  let __pipe_8 = (\ (x) -> x) _lmymensnxnpt
+  let _sxvvldpoxgyq = _Count (__pipe_8)
+  let __pipe_10 = (\ (x) -> x) _sxvvldpoxgyq
+  let _qvgqbhxirhfu = return __pipe_10
+  let __pipe_11 = (\ (x) -> x) _qvgqbhxirhfu
+  let _Return1 = _Return (__pipe_11)
+  _Return1
