7dd953a38cea21e7b7154336d59fd81f3e47da0e
Scott Lawrence <bytbox@gmail.com>
2012-01-25 23:13:47 -0500

    genTools basically working. Minimum functionality only - no documentation generation, and the python code is unaware of the actual types - only the categories and numbers of inputs and ouputs are revealed.

--- gui/genTools.pl
+++ gui/genTools.pl
@@ -5,18 +5,32 @@ use File::Basename qw/dirname/;
 use strict;
 use warnings;
 
+sub ptup;
+
 my $ed = dirname $0;
 my $pdir = $ARGV[0] || "$ed/../lib/EVAN";
 my @hsfiles = glob "$pdir/*.lhs";
 for my $f (@hsfiles) {
 	$f =~ /\/(\w+)\.lhs/;
-	print "== $1\n";
+	print "add_category('$1', [\n";
 	open FIN, $f or die $!;
 	my $hs = "";
 	$hs .= $_ while <FIN>;
 	close FIN or die $!;
 	while ($hs =~ /^> *_([A-Z]\w*) *:: *(.*=> *)?(.*?) *-> *(.*)/mg) {
-		my ($name, $args, $res) = ($1, $3, $4);
-		print "$name :: $args -> $res\n";
+		my ($name, $argstr, $restr) = ($1, $3, $4);
+		my @args = ptup $argstr;
+		my @res = ptup $restr;
+		my ($al, $rl) = ($#args+1, $#res+1);
+		print "    ('$name', $al, $rl),\n";
 	}
+	print "])\n";
+}
+
+sub ptup {
+	my $as = shift;
+	$as =~ s/^\( *//;
+	$as =~ s/ *\)$//;
+	my @args = split /,/, $as;
+	return @args;
 }
