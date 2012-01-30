0e3ffb1e45e0564f7d39b152de981f1030d04b8f
Scott Lawrence <bytbox@gmail.com>
2012-01-29 21:12:54 -0500

    Adding more verbose build.sh and checks for python dependencies

--- build.sh
+++ build.sh
@@ -2,16 +2,20 @@
 
 set -e
 
+echo Preparing python GUI...
+python -c 'import matplotlib; import numpy; import tkinter'
 pushd . > /dev/null
 cd gui
 ./genTools.pl
 popd > /dev/null
 
+echo Installing EVAN haskell libraries...
 pushd . > /dev/null
 cd lib
 cabal install
 popd > /dev/null
 
+echo Populating bin/...
 mkdir -p bin
 rm -f bin/*
 ln -s `pwd`/json2hs/json2hs.py bin/json2hs
