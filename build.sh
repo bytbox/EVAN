fe2b38fe257b2dcb5066317ec930d52c8edb9d16
Scott Lawrence <bytbox@gmail.com>
2012-01-31 20:22:58 -0500

    Moving json2hs to tools

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ popd > /dev/null
 echo Populating bin/...
 mkdir -p bin
 rm -f bin/*
-ln -s `pwd`/json2hs/json2hs.py bin/json2hs
+ln -s `pwd`/tools/json2hs/json2hs.py bin/json2hs
 ln -s `pwd`/scripts/evan-compile bin/evan-compile
 tools/merge.pl gui/evan.py > bin/evan
 chmod +x bin/evan
