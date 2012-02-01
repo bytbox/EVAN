f870b356da09999f9463c0c6dd0b96352f67c31a
Scott Lawrence <bytbox@gmail.com>
2012-01-31 20:16:40 -0500

    Removing unwanted code

--- lib/Main.hs
+++
@@ -1,5 +0,0 @@
-module Main where
-import EVAN
-import EVAN.Primitive
-import EVAN.Math
-main = do { let {_Events1 = _Events () ; __pipe_5 = (\ (x) -> x) _Events1;_qvgqbhxirhfu = do {_rpsdfspgrjnu <- __pipe_5 ; let {__pipe_6 = (\ (x) -> x) _rpsdfspgrjnu;_lmymensnxnpt = _Jets (__pipe_6) ; __pipe_8 = (\ (x) -> x) _lmymensnxnpt;_sxvvldpoxgyq = _Count (__pipe_8) ; __pipe_10 = (\ (x) -> x) _sxvvldpoxgyq;} ; return __pipe_10} ; __pipe_11 = (\ (x) -> x) _qvgqbhxirhfu;_Return1 = _Return (__pipe_11) ; }; _Return1 }
