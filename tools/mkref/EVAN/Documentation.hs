bbecc16759d53208384d31568f37d33ebcb2e63f
Scott Lawrence <bytbox@gmail.com>
2012-02-01 22:25:40 -0500

    Preparing to parse categories

--- tools/mkref/EVAN/Documentation.hs
+++ tools/mkref/EVAN/Documentation.hs
@@ -7,7 +7,7 @@ import Text.JSON
 
 data Docs = Docs
   { generated   :: String
-  , categories  :: Map String Function
+  , categories  :: Map String [Function]
   }
 
 data Function = Function
@@ -18,15 +18,21 @@ data Function = Function
 instance JSON Docs where
   readJSON (JSObject jso) = do
     JSString generatedStr <- lkup "generated"
+    categoriesJSON <- lkup "categories"
+    categories <- readJSON categoriesJSON
     return $ Docs
       { generated = fromJSString generatedStr
-      , categories = Map.fromList []
+      , categories = Map.fromList categories
       }
     where
       obj = Map.fromList $ fromJSObject jso
       lkup k = case Map.lookup k obj of
                 Just v -> Ok v
                 Nothing -> Error ("key not found: " ++ k)
+  showJSON = undefined
+
+instance JSON Function where
+  readJSON = const $ Ok Function
   showJSON = undefined
 
 readDocs :: String -> Either Docs String
