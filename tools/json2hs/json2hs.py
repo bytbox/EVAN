8b72ac9d08e453ef6bf2e0dbfaad024bcbe7381e
Scott Lawrence <bytbox@gmail.com>
2012-03-19 15:23:53 -0400

    Fixing build for things not using params at all

--- tools/json2hs/json2hs.py
+++ tools/json2hs/json2hs.py
@@ -49,6 +49,7 @@ def render(p, i):
                 deps += dep
                 ds += d
             r += rid(ident) + ' ('
+            r += ','.join(['_'+x for x in o['params']]) + ') ('
             r += ','.join(['_'+x for x in o['inputs']]) + ') ; '
             r = ds + r
     elif k == 'pipe':
