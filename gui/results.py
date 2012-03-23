068e9e4e225eefac6e59dbddf7a100c4b1aabf94
Scott Lawrence <bytbox@gmail.com>
2012-03-23 14:46:55 -0400

    Creating python code to attempt to read and display images

--- gui/results.py
+++ gui/results.py
@@ -1,23 +1,24 @@
 # Displaying results for the python+tk frontend. See README for notes.
 
 import json
+import re
 from tkinter import *
 
 def display_results(rs):
     """ Displays the given results. """
 
     r = json.loads(rs)
+    root = Tk()
+    root.title('Results')
+    root.protocol('WM_DELETE_WINDOW', root.destroy)
+
+    resFrame = Frame(root)
+    resFrame.pack(side=LEFT, anchor='nw', fill=BOTH, expand=1)
+
+    resDisp = Canvas(resFrame, bg='white', bd=2, relief=SUNKEN)
+    resDisp.pack(side=LEFT, anchor='nw', fill=BOTH, expand=1)
     if isinstance(r, str):
-        root = Tk()
-        root.title('Results')
-        root.protocol('WM_DELETE_WINDOW', root.destroy)
-
-        resFrame = Frame(root)
-        resFrame.pack(side=LEFT, anchor='nw', fill=BOTH, expand=1)
-
-        resDisp = Canvas(resFrame, bg='white', bd=2, relief=SUNKEN)
-        resDisp.pack(side=LEFT, anchor='nw', fill=BOTH, expand=1)
-        resDisp.create_text(20, 20, text=r)
+       resDisp.create_text(20, 20, text=r)
     elif isinstance(r, dict):
         m = r['mime']
         k = r['kind']
@@ -30,6 +31,13 @@ def display_results(rs):
             f.close()
         else:
             # TODO ERROR
+            return
+
+        if re.match('^image/', m):
+            img = PhotoImage(data=d)
+            resDisp.create_image(30, 30, image=img)
+        else:
+            # TODO error
             return
     else:
         print(r)
