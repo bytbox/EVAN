287b6c55552eb24a51844f92820d1569beba570d
Scott Lawrence <bytbox@gmail.com>
2012-03-25 12:58:51 -0400

    Menu and toolbar stubs

--- gui/main.cc
+++ gui/main.cc
@@ -1,24 +1,47 @@
 #include "fltk.hh"
 
+#define TITLE "EVAN"
 #define WIDTH 680
 #define HEIGHT 580
-#define TITLE "EVAN"
+#define MENU_HEIGHT 28
+#define TOOL_HEIGHT 30
+
+/* Callbacks */
+void Cb_Quit (Fl_Widget *, void *);
 
 /* Singleton GUI Components */
 Fl_Double_Window *main_window;
 Fl_Menu_Bar *menu_bar;
-Fl_Menu_Button *file_menu, *help_menu;
-Fl_Pack *toolbar;
+Fl_Pack *tool_bar;
+Fl_Button *run_button;
+
+Fl_Menu_Item menu_items[] = {
+	{ "&File",              0, 0, 0, FL_SUBMENU },
+	{ "E&xit", FL_COMMAND + 'q', (Fl_Callback *)Cb_Quit, 0 },
+	{ 0 },
+
+	{ "&Help", 0, 0, 0, FL_SUBMENU },
+	{ 0 },
+
+
+	{ 0 }
+};
 
 void MkGui() {
 	main_window = new Fl_Double_Window(WIDTH, HEIGHT, TITLE);
+	menu_bar = new Fl_Menu_Bar(0, 0, WIDTH, MENU_HEIGHT);
+	menu_bar->copy(menu_items);
 
-	/* Menus */
-	menu_bar = new Fl_Menu_Bar(0, 0, 40, 40);
-
-	
+	tool_bar = new Fl_Pack(0, MENU_HEIGHT+1, 80, 0);
+	run_button = new Fl_Button(0, 0, 0, TOOL_HEIGHT, "Run");
+	tool_bar->end();
 
 	main_window->end();
+}
+
+void Cb_Quit(Fl_Widget *, void *) {
+	delete main_window;
+	exit(0);
 }
 
 int main(int argc, char *argv[]) {
@@ -26,3 +49,4 @@ int main(int argc, char *argv[]) {
 	main_window->show(argc, argv);
 	return Fl::run();
 }
+
