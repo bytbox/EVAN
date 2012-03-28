a30f80c5cc66c23599c8dc3d3bb4a9d05877ef5f
Scott Lawrence <bytbox@gmail.com>
2012-03-28 00:01:28 -0400

    evan2hs parsing works

--- tools/evan2hs/evan2hs.hs
+++ tools/evan2hs/evan2hs.hs
@@ -1,16 +1,60 @@
 module Main where
 
-import Text.ParserCombinators.Parsec
+import Data.Maybe
+import Text.Parsec
+
+data Ident = Ident String
+  deriving (Eq, Show)
+
+data Expr = Id Ident
+  deriving (Eq, Show)
+
+data Statement = Assign Ident Expr | Each Ident Expr [Statement]
+  deriving (Eq, Show)
 
 skip p = p >> return ()
 
-comment = skip $ char '['
+comment = do
+  string "[["
+  manyTill anyChar $ string "]]"
+  return Nothing
 
 tokEach = skip $ string "each"
+ident = many1 letter >>= return . Ident
+rarrow = skip $ string "<-"
+expr = ident >>= return . Id
 
-statement = tokEach <|> comment
+assign = do
+  d <- ident
+  skipMany space
+  rarrow
+  skipMany space
+  s <- expr
+  return $ Just (Assign d s)
 
-parser = statement
+each = do
+  tokEach
+  skipMany1 space
+  d <- ident
+  skipMany space
+  rarrow
+  skipMany space
+  s <- expr
+  skipMany space
+  l <- between (char '{') (char '}') stmtList
+  return $ Just $ Each d s l
+
+statement = try assign <|> try each
+
+stmtList = do
+  l <- many (try $ skipMany space >> (comment <|> statement))
+  skipMany space
+  return $ catMaybes l
+
+parser = do
+  l <- stmtList
+  eof
+  return l
 
 parseFile f c = runParser parser () f c
 
