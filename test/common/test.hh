c574ce5b7e1c3f9b31c1e86ef2876f9f3154a95e
Scott Lawrence <bytbox@gmail.com>
2012-07-08 21:55:05 -0700

    Tests for maybe

--- test/common/test.hh
+++ test/common/test.hh
@@ -1,4 +1,9 @@
+#ifndef TEST_HH
+#define TEST_HH
+
 class test {
 public:
 };
+
+#endif /* !TEST_HH */
 
--- test/util/maybe.cc
+++ test/util/maybe.cc
@@ -1,8 +1,14 @@
 #include "test.hh"
 
 #include "util.hh"
+using util::maybe;
+
+#include <cassert>
 
 int main(int argc, char *argv[]) {
+	assert(!maybe<int>().isDefined());
+	assert(maybe<int>(3).isDefined());
+	assert(maybe<int>(3).get() == 3);
 	return 0;
 }
 
