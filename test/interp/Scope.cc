fe210916da471fef6d0f102de51d4c162ba1a525
Scott Lawrence <bytbox@gmail.com>
2012-07-09 06:12:05 -0700

    tests for interpreter::scope

--- test/interp/Scope.cc
+++ test/interp/Scope.cc
@@ -5,6 +5,13 @@
 #include <cassert>
 
 void testmain() {
-
+	Interpreter::Scope s = Interpreter::Scope().into();
+	assert (s == s);
+	assert (s.next() == s.next());
+	assert (s.into() == s.into());
+	assert (s != s.next());
+	assert (s != s.into());
+	assert (s.next() != s.into().next());
+	assert (s.next().into() != s.into().next());
 }
 
