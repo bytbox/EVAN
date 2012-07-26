74f797cb56dbc2061e085bb350326ad33fbf62f4
Scott Lawrence <bytbox@gmail.com>
2012-07-25 20:20:35 -0700

    Adding stub tests

---
+++ test/core/FieldExtra.cc
@@ -0,0 +1,7 @@
+#include "test.hh"
+
+#include "program.hh"
+
+namespace _FieldExtra {
+suite s("FieldExtra", module::get("core"));
+};
---
+++ test/util/strconv.cc
@@ -0,0 +1,7 @@
+#include "test.hh"
+
+#include "util.hh"
+
+namespace _strconv {
+suite s("strconv", module::get("util"));
+};
