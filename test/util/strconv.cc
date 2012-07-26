c76e5cbbe36174625cadd259bb6e67751e35a8e0
Scott Lawrence <bytbox@gmail.com>
2012-07-25 20:40:12 -0700

    A strconv test

--- test/util/strconv.cc
+++ test/util/strconv.cc
@@ -1,7 +1,16 @@
 #include "test.hh"
 
 #include "util.hh"
+using util::asString;
+using util::ofString;
+
+#include <cassert>
 
 namespace _strconv {
 suite s("strconv", module::get("util"));
+test t1("int2string", s, ([](){
+	assert(asString(1) == "1");
+	assert(asString(0) == "0");
+	assert(asString(105) == "105");
+}));
 };
