82d9e2420ed383ebf563d7af8070e2147a9e09e6
Scott Lawrence <bytbox@gmail.com>
2012-08-02 20:10:54 -0700

    registry test stubs

---
+++ test/util/composite_registry.cc
@@ -0,0 +1,9 @@
+#include "test.hh"
+
+#include "util.hh"
+using util::composite_registry;
+
+namespace _composite_registry {
+SUITE("composite_registry", "util");
+};
+
---
+++ test/util/simple_registry.cc
@@ -0,0 +1,9 @@
+#include "test.hh"
+
+#include "util.hh"
+using util::simple_registry;
+
+namespace _simple_registry {
+SUITE("simple_registry", "util");
+};
+
