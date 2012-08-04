ce60608c4fc5c304b6ea4571f8f902bf23a73953
Scott Lawrence <bytbox@gmail.com>
2012-08-04 15:16:15 -0700

    Stubbing out the type system

--- include/typecheck.hh
+++ include/typecheck.hh
@@ -1,7 +1,42 @@
 #ifndef TYPECHECK_HH
 #define TYPECHECK_HH
 
+#include <vector>
+using namespace std;
+
+/*!
+ * \brief Represents the type of a Value.
+ *
+ * Functions are not first-class objects, so the type of a function is not a
+ * Type.
+ */
 class Type {
+public:
+};
+
+/*!
+ * \brief A native (or built-in) type.
+ */
+class NativeType : public Type {
+public:
+};
+
+class ForeignType : public Type {
+public:
+};
+
+class VariableType : public Type {
+public:
+};
+
+/*!
+ * Because blocks (functions) are not first class objects, BlockType does not
+ * implement Type.
+ */
+class BlockType {
+	std::vector <Type *> parameters;
+	std::vector <Type *> arguments;
+	std::vector <Type *> results;
 public:
 };
 
