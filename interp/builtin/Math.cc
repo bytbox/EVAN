166f6b23e97867f7632ca83c4bbbf6093bcfd8da
Scott Lawrence <bytbox@gmail.com>
2012-08-05 09:59:21 -0700

    Sum and Product

--- interp/builtin/Math.cc
+++ interp/builtin/Math.cc
@@ -31,11 +31,47 @@ Value Div(vector <Param> ps, vector <Value> vs) {
 	return double(vs[0]) / double(vs[1]);
 }
 
+Value Sum(vector <Param> ps, vector <Value> vs) {
+	// Note that all elements in vs[0] will in fact have the same type, so
+	// we can decide our behaviour based on the first one.
+	list <Value> l = vs[0].lst();
+	if (l.size() == 0) return double(0);
+	if (l.front().type == Value::INT) {
+		int sum = 0;
+		for (Value v : l)
+			sum += int(v);
+		return sum;
+	} else {
+		double sum = 0;
+		for (Value v : l)
+			sum += double(v);
+		return sum;
+	}
+}
+
+Value Product(vector <Param> ps, vector <Value> vs) {
+	list <Value> l = vs[0].lst();
+	if (l.size() == 0) return double(1);
+	if (l.front().type == Value::INT) {
+		int prod = 1;
+		for (Value v : l)
+			prod *= int(v);
+		return prod;
+	} else {
+		double prod = 1;
+		for (Value v : l)
+			prod *= double(v);
+		return prod;
+	}
+}
+
 simple_registry<Interpreter::Function> Interpreter::mathFunctions
 ({	{"Add", &Add},
 	{"Sub", &Sub},
 	{"Mul", &Mul},
 	{"Div", &Div},
+	{"Sum", &Sum},
+	{"Product", &Product},
 	{"Lt", (IFUNC { return double(vs[0]) < double(vs[1]); })},
 	{"Le", (IFUNC { return double(vs[0]) <= double(vs[1]); })},
 	{"Gt", (IFUNC { return double(vs[0]) > double(vs[1]); })},
