795a683740fadd5cf6274eddf00302d69d3b4312
Scott Lawrence <bytbox@gmail.com>
2012-08-11 13:34:46 -0700

    Parsing working and tested

--- test/unit/foreign/lhco.cc
+++ test/unit/foreign/lhco.cc
@@ -10,5 +10,9 @@ SUITE("LHCO", "foreign");
 test t1("parse", s, ([](){
 	Vec_Foreign events = LHCO_Input("foreign/data/ttjj.lhco");
 	assert(events.len == 6);
+	Vec_Foreign ev0 = LHCO_Parts(events.data[0]);
+	assert(ev0.len == 8);
+	Vec_Foreign ev1 = LHCO_Parts(events.data[1]);
+	assert(ev1.len == 7);
 }));
 };
