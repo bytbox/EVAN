b19c5491ca67d28716a32c9330190bc8fad99aa7
Scott Lawrence <bytbox@gmail.com>
2012-08-17 05:49:21 -0700

    More foreigns.hh stubs

---
+++ core/ForeignCategory.cc
@@ -0,0 +1,2 @@
+#include "foreigns.hh"
+
---
+++ include/foreigns.hh
@@ -0,0 +1,23 @@
+#ifndef FOREIGNS_HH
+#define FOREIGNS_HH
+
+#include "typecheck.hh"
+
+#include <string>
+#include <vector>
+
+class ForeignFunc {
+public:
+};
+
+class ForeignCategory {
+public:
+};
+
+class ForeignInfo {
+public:
+	static const std::vector <ForeignCategory> categories;
+};
+
+#endif /* !FOREIGNS_HH */
+
