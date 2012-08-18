567f18c3ac4018d1518c8f8861d3711d4c1f8bcd
Scott Lawrence <bytbox@gmail.com>
2012-08-18 15:39:04 -0700

    output_method_name and output_method_from_name

--- include/output.h
+++ include/output.h
@@ -1,5 +1,10 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+typedef enum {PLAIN, PLOT, ROOT} output_method_t;
+
+const char *output_method_name(output_method_t);
+output_method_t output_method_from_name(const char *);
+
 #endif /* !OUTPUT_H */
 
--- output/output.c
+++ output/output.c
@@ -1,2 +1,19 @@
 #include "output.h"
 
+#include <string.h>
+
+const char *output_method_names[] = {"plain", "plot", "root", 0};
+
+const char *output_method_name(output_method_t meth) {
+	return output_method_names[meth];
+}
+
+output_method_t output_method_from_name(const char *name) {
+	const char **np = output_method_names;
+	while (*np) {
+		if (!strcasecmp(name, *np))
+			return np - output_method_names;
+	}
+	return -1;
+}
+
