a3eee87d0a1a0ee3612afd3e2c9466d71d0e417b
Scott Lawrence <bytbox@gmail.com>
2012-08-19 10:19:15 -0700

    Actually using output methods in evani

--- cli/evani.cc
+++ cli/evani.cc
@@ -1,7 +1,7 @@
-#include "parse.hh"
-
 #include "interp.hh"
+#include "parse.hh"
 #include "program.hh"
+#include "output.h"
 #include "util.hh"
 using namespace util;
 
@@ -16,13 +16,42 @@ int main(int argc, char *argv[]) {
 	if (args.flag("h") || args.flag("help")) {
 		cout << "usage: " << argv[0] << " [options]" << endl;
 		cout << "options:" << endl;
+		cout << "  -output=METHOD         use the specified output method" << endl;
+		cout << "  -output-opts=OPTS      options for the selected output method" << endl;
+		cout << "  -output-dest=FILENAME  write output to the specified file" << endl;
 		return 0;
 	}
 	try {
 		Program *program = parseProgram(stdin);
 		ProgramInterpreter *pi = new ProgramInterpreter(program);
 		auto v = pi->next(Interpreter::Scope::empty.into()).get();
-		cout << v.toString() << endl;
+
+		output_method_t meth = output_method_from_name(args.opt("output", "plain").c_str());
+		const char *outopts = args.opt("output-opts", "").c_str();
+		string outfname = args.opt("output-dest", "");
+		output_destination_t dest;
+		if (outfname == "") dest = output_to_stdout();
+		else dest = output_to_basename(outfname.c_str());
+
+		switch (v.type) {
+		case Value::BOOL:
+			output_number(meth, dest, NULL, ((bool)v)?1:0);
+			break;
+		case Value::INT:
+		case Value::FLOAT:
+			output_number(meth, dest, NULL, v.asDouble());
+			break;
+		case Value::VEC:
+		case Value::LIST:
+			break;
+		case Value::FOREIGN:
+			cerr << "Can't display foreign values" << endl;
+			// TODO
+			break;
+		case Value::BOT:
+		default:
+			throw (new impossible_error())->with(_POS);
+		}
 	} catch (string s) {
 		cerr << "Caught: " << s << endl;
 	} catch (util::error *err) {
--- include/output.h
+++ include/output.h
@@ -1,6 +1,10 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 
 typedef enum {PLAIN, PLOT, ROOT, ASCII} output_method_t;
@@ -25,6 +29,10 @@ void output_number(output_method_t, output_destination_t, const char **, double)
 void output_bars(output_method_t, output_destination_t, const char **, int, double[]);
 void output_histogram(output_method_t, output_destination_t, const char **, int, double[]);
 void output_histogram2d(output_method_t, output_destination_t, const char **, int, int, double[]);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !OUTPUT_H */
 
