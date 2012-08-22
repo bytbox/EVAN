bb7c0239ccafaf02adedadeed91249a830f9d88b
Scott Lawrence <bytbox@gmail.com>
2012-08-21 22:11:49 -0700

    Some ASCII stubs

--- output/ascii.c
+++ output/ascii.c
@@ -1,2 +1,31 @@
+#include "histogram.h"
 #include "output.h"
+#include "methods.h"
+
+#include <stdio.h>
+
+void ascii_number(FILE *f, const char **opts, double d) {
+	fprintf(f, "%f\n", d);
+}
+
+void ascii_bars(FILE *f, const char **opts, int sz, double d[]) {
+	int i;
+	for (i=0; i<sz; i++)
+		fprintf(f, "%f\n", d[i]);
+}
+
+void ascii_histogram(FILE *f, const char **opts, int sz, double d[]) {
+	struct histogram_opts hist = histogram(1, 0);
+	make_histogram(&hist, sz, d);
+	int i;
+	double c = hist.first;
+	for (i = 0; i < hist.count; i++) {
+		fprintf(f, "%f: %d\n", c, hist.hist[i]);
+		c += hist.width;
+	}
+}
+
+void ascii_histogram2d(FILE *f, const char **opts, int xsz, int ysz, double d[]) {
+	// TODO
+}
 
--- output/methods.h
+++ output/methods.h
@@ -8,5 +8,10 @@ void plain_bars(FILE *, const char **, int, double[]);
 void plain_histogram(FILE *, const char **, int, double[]);
 void plain_histogram2d(FILE *, const char **, int, int, double[]);
 
+void ascii_number(FILE *, const char **, double);
+void ascii_bars(FILE *, const char **, int, double[]);
+void ascii_histogram(FILE *, const char **, int, double[]);
+void ascii_histogram2d(FILE *, const char **, int, int, double[]);
+
 #endif /* !METHODS_H */
 
